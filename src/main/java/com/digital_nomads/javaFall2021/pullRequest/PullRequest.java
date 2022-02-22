f30ac1a0f6d42aa89a6afaa8c3ed79c3715254a2
urmatMaratov <umakevicc@gmail.com>
2022-02-22 19:47:49 +0600

    Added pull request

---
+++ src/main/java/com/digital_nomads/javaFall2021/pullRequest/PullRequest.java
@@ -0,0 +1,7 @@
+package com.digital_nomads.javaFall2021.pullRequest;
+
+public class PullRequest {
+    public static void main(String[] args) {
+        System.out.println("testing pull request");
+    }
+}
